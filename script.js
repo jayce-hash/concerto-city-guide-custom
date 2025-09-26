9f3146912b53a78980d42658c158249d4b0f75c3
jayce-hash <jayce@concertocity.com>
2025-09-26 11:49:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -92,6 +92,7 @@ const instruction = document.createElement('p');
 instruction.textContent = 'Select a place below to learn more';
 instruction.style.fontSize = '14px';
 instruction.style.color = '#5e6b86';
+instruction.style.marginTop = '10px';
 instruction.style.marginBottom = '12px';
 instruction.style.textAlign = 'center';
 resultsContainer.appendChild(instruction);
